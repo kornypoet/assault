b05935fa2b19f26b3b7d450b3e6efde3c3497eb2
Travis Dempsey <travis@infochimps.com>
2011-02-02 19:20:51 -0600

    added basic features and step definitions

---
+++ features/step_definitions/program_steps.rb
@@ -0,0 +1,11 @@
+Given /^The User has installed The Game of Assault$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^The User first runs the Program$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^The Program should make sure the User has all dependant Gems installed$/ do
+  pending # express the regexp above with the code you wish you had
+end
